9d76c3b50c7ef4f152c61ea160a00780a7ecd233
SoulTaker9090 <110356336+SoulTaker9090@users.noreply.github.com>
2023-08-21 22:37:54 +0000

    finished area calculator working or min max value

--- calcualtion_area.py
+++ calcualtion_area.py
@@ -23,20 +23,44 @@ def shape(question):
       return shape
     else:
           print(error)
-def area_calculator(question,shape):
+# area calculator
+def area_calculator(shape):
   error = "please enter a integer"
   area = 0
   pi = 3.14
-  if shape == "square":
-    one_side = int(input("please input one i the sides mesurement"))
-    area = area + (one_side ** 2)
-    return area
-  elif shape == "circle":
-    radius = int(input("Please enter the value of your radius"))
-    area = area + (pi * radius ** 2)
-    return area
-  elif shape == "rectangle":
-    
-# calls on the program   
-picker = shape("please pick either square triangle or rectangle:")
-print(picker)
+  valid = False
+  while not valid:
+    try:
+      if shape == "square":
+        one_side = int(input("Please input one of the sides mesurement from you square:"))
+        area = area + (one_side ** 2)
+        return area
+      elif shape == "circle":
+        radius = int(input("Please enter the value of your radius of your circle:"))
+        area = area + (pi * radius ** 2)
+        return area
+      elif shape == "rectangle":
+        length = int(input("Please enter the value of legth of the rectangle:"))
+        width = int(input("Please enter the value of the width of the rectangle:"))
+        area = area + (length * width)
+        return area
+      elif shape == "triangle":
+        Tbase = int(input("Please enter the base of your trinagle:"))
+        Theight = int(input("Please enter your height of the tringle:"))
+        area = area + (0.5 * Tbase * Theight)
+        return area
+      elif shape == "parrallelagram":
+        Pbase = int(input("Please enter the base of the parrallelagram:"))
+        Pheight = int(input("Please enter the height of your parrallelagram:"))
+        area = area + ( Pbase * Pheight)
+        return area
+    except ValueError:
+        print(error)
+def max_vale(area_calculator):
+  
+# calls on the program
+  picker = shape("Please pick either square triangle rectangle circle or parrallelagram:")
+  print(picker)
+  area = area_calculator(picker)
+  print("{:.2f}".format(area))
+  
